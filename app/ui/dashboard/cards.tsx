9a08cfbca1fc41092d5aa5bfedb6f58c81a74f26
Brian Seegmiller <brianseegmiller@gmail.com>
2023-11-24 07:58:23 -0700

    chapter 7 data fetching

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,10 +1,10 @@
+import { lusitana } from '@/app/ui/fonts';
 import {
   BanknotesIcon,
   ClockIcon,
-  UserGroupIcon,
   InboxIcon,
+  UserGroupIcon,
 } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -34,11 +34,11 @@ export function Card({
   title,
   value,
   type,
-}: {
+}: Readonly<{
   title: string;
   value: number | string;
   type: 'invoices' | 'customers' | 'pending' | 'collected';
-}) {
+}>) {
   const Icon = iconMap[type];
 
   return (
